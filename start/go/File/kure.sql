b9d28d4454fede82c66f09daa33f011492858d74
Hannes Ehrensperger <Ziitlos@users.noreply.github.com>
2017-07-19 08:14:16 +0200

    Add files via upload

--- start/go/File/kure.sql
+++ start/go/File/kure.sql
@@ -1,43 +1,29 @@
--- kure.sql
-
 select u.Id,
 u.UserName,
 u.FirstName,
 u.LastName,
 u.EMail,
-u.ReplacementEMail,
-u.ResetToken,
-u.ResetType,
-u.ResetTime,
+u.EMailState,
+ur.Name as UserRoleName,
+u.HelsanaEmployee,
 u.ClientTermsAgreement,
 u.ClientTermsAgreementTimestamp,
 u.NonClientTermsAgreement,
 u.NonClientTermsAgreementTimestamp,
-u.HelsanaNewsServices,
-u.Version,
 u.CreatedBy,
 u.CreatedOn,
 u.UpdatedBy,
 u.UpdatedOn,
-u.Deleted,
-u.EMailState,
-u.UserRoleId,
 u.StatusPoints,
 u.BonusPoints,
 u.LocaleId,
 u.DeviceId,
 u.AddressId,
 u.Iban,
-u.HelsanaEmployee,
 u.Gender,
-u.SendNotification,
-ur.Id,
-ur.Name as UserRoleName,
-ur.Version as UserRoleVersion,
-ur.YearLimit,
 ur.MaxPoints
-from dbo.HelsanaUsers u,
-     dbo.UserRoles ur
+from Helsana_prod.dbo.HelsanaUsers u,
+     Helsana_prod.dbo.UserRoles ur
 where ur.Id = u.UserRoleId
 and ur.Deleted = 0
 and u.Deleted = 0
